a4bda4c81d94a53c68918b5df1de8e4b19755503
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-25 10:57:44 +0000

    variables

--- variables/Program.cs
+++ variables/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = 1234567890;
+double bigNum = 1.23456789012345e+20;
+string message = "the quick brown fox jumps over the lazy dog";
+float pi = 3.14159f;
+decimal price = 19.99m;
+char letter = 'A';
+bool isActive = true;
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+
