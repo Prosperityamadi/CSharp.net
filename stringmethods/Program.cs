23dab292b7de86fed204997a5553f75d262eb8d0
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 18:48:01 +0000

    Refactor Program.cs to implement string manipulation and output

--- stringmethods/Program.cs
+++ stringmethods/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String fullName = "Bro Code";
+            String phoneNumber = "123-456-7890";
+
+            //fullName = fullName.ToUpper();
+            //fullName = fullName.ToLower();
+            //Console.WriteLine(fullName);
+
+            //phoneNumber = phoneNumber.Replace("-","");
+            //Console.WriteLine(phoneNumber);
+
+            //String userName = fullName.Insert(0,"Mr.");
+            //Console.WriteLine(userName);
+
+            //Console.WriteLine(fullName.Length);
+
+            String firstName = fullName.Substring(0, 3);
+            String lastName = fullName.Substring(4, 4);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            Console.ReadKey();
+        }
+    }
+}
