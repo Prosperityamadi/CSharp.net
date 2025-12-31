d41c4ebe6464e244a42514b58645a412ec21a713
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 18:35:33 +0000

    Implement number guessing game in Program.cs

--- randomnumbers/Program.cs
+++ randomnumbers/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Random random = new Random();
+            int randomNumber = random.Next(1,10);
+
+            Console.WriteLine("Enter a number between 1 and 10:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == randomNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed the correct number.");
+            }else
+            {
+                Console.WriteLine($"Sorry, the correct number was {randomNumber}. Better luck next time!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
