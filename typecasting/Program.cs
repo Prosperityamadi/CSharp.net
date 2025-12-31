888a4fb9fb0d283ac35f733e63f1bf9f0c1a8828
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 12:18:32 +0000

    Comment out implicit and explicit conversion examples in Program.cs

--- typecasting/Program.cs
+++ typecasting/Program.cs
@@ -3,14 +3,28 @@
 
 //implicit conversion example
 
-int bigNumber = 123456;
-long biggerNumber = bigNumber; // implicit conversion from int to long
+// int bigNumber = 123456;
+// long biggerNumber = bigNumber; // implicit conversion from int to long
 
-Console.WriteLine($"Implicit Conversion: int {bigNumber} to long {biggerNumber}");
+// Console.WriteLine($"Implicit Conversion: int {bigNumber} to long {biggerNumber}");
 
-//explicit conversion example
+// //explicit conversion example
 
-double decimalNumber = 123.45;
-int wholeNumber = (int)decimalNumber; // explicit conversion from double to int
+// double decimalNumber = 123.45;
+// int wholeNumber = (int)decimalNumber; // explicit conversion from double to int
 
-Console.WriteLine($"Explicit Conversion: double {decimalNumber} to int {wholeNumber}");
+// Console.WriteLine($"Explicit Conversion: double {decimalNumber} to int {wholeNumber}");
+ 
+// advanced type casting
+
+// string to int
+string numberString = "123456";
+int result = 0;
+if (int.TryParse(numberString, out result))
+{
+    Console.WriteLine($"String to Int Conversion: string '{numberString}' to int {result}");
+}
+else
+{
+    Console.WriteLine($"Conversion failed for string '{numberString}'");
+}
