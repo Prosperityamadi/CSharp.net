0db618f62c2c734e971426617fb433c9e90b64d4
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 14:24:42 +0000

    Refactor comments and formatting in Program.cs for clarity on stack and heap memory allocation

--- stackandheap/Program.cs
+++ stackandheap/Program.cs
@@ -1,1 +1,15 @@
 ﻿// stack and heap memory allocation in C#
+
+// stack is for value types which are stored directly in the stack frame 
+// while heap is for reference types which are stored in the heap memory
+
+
+using System;
+
+int testValue = 4;
+byte lowLevelValue = 3;
+bool booleanValue = false;
+string stringValue = "Hello, World!";
+
+StackHeapExample stackHeap = new StackHeapExample();
+stackHeap.AddFive(5);
