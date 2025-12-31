a072eec0d7676582552cae5dda594df59ab737e2
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 17:48:57 +0000

    Add initial project files for arrays .NET application

---
+++ input/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name:");
+        String name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+
+        Console.WriteLine("Please enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"You are {age} years old.");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
