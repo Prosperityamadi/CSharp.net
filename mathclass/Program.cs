f32afec0a2f18ef58b2ee8c7bf2e0584a9ace4f1
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-12-31 18:01:24 +0000

    Refactor Program.cs to implement cube calculation and output

--- mathclass/Program.cs
+++ mathclass/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO.Pipelines;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 5;
+
+            double result = Math.Pow(num, 3);
+            Console.WriteLine($"The cube of {num} is {result}");  
+
+            Console.ReadKey();
+        }
+    }
+}
