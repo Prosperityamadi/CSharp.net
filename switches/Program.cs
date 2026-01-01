48a3ae32fde359261dc5063460e19084bdf784b8
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2026-01-01 14:14:43 +0000

    Implement day-of-week switch statement in Program.cs

--- switches/Program.cs
+++ switches/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // switches are effective for handling multiple discrete values
+            Console.WriteLine("What day is it today?");
+            string? day = DateTime.Now.DayOfWeek.ToString();
+            day = Console.ReadLine();
+
+            switch (day)
+            {
+                case ("monday"):
+                    Console.WriteLine("It's Monday!");
+                    break;
+                case ("tuesday"):
+                    Console.WriteLine("It's Tuesday!");
+                    break;
+                case ("wednesday"):
+                    Console.WriteLine("It's Wednesday!");
+                    break;
+                case ("thursday"):
+                    Console.WriteLine("It's Thursday!");
+                    break;
+                case ("friday"):
+                    Console.WriteLine("It's Friday!");
+                    break;
+                case ("saturday"):
+                    Console.WriteLine("It's Saturday!");
+                    break;
+                case ("sunday"):
+                    Console.WriteLine("It's Sunday!");
+                    break;
+                default:
+                    Console.WriteLine("Hmm, that doesn't seem to be a valid day.");
+                    break;
+            }
+            Console.WriteLine("Have a great day!");
+        }
+    }
+}
