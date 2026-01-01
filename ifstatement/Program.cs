ee2719c743f52f48c6881c7f4e776ff63d2823cd
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2026-01-01 13:52:05 +0000

    Implement user name input and greeting in Program.cs

--- ifstatement/Program.cs
+++ ifstatement/Program.cs
@@ -5,6 +5,17 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please what is your name?");
+            string name = Console.ReadLine();
+
+            if(name == null || name.Trim() == "")
+            {
+                Console.WriteLine("please enter a valid name");
+            }
+            else
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
             Console.ReadKey();
         }
     }
